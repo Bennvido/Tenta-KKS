1e13103b77c730453367d75c6757a692072187e8
bennvido <marcus.bennvid@medieinstitutet.se>
2020-05-15 10:39:29 +0200

    CPT katter arkiv
    vad som ska skrivas ut från cats i archive-cats

---
+++ archive-cats.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+?>
+<div class="container">
+	<div class="row">
+    <!-- Loop start	 -->
+    <?php if (have_posts()) : ?>
+        <!-- Yay, we have posts  -->
+        <?php while (have_posts()) : the_post(); ?>
+		
+            <!-- Accordion of FAQ's -->
+            <?php get_template_part('loop-templates/content', 'cats'); ?>
+            <!-- End of Accordion -->
+		
+        <?php endwhile; ?>
+    <?php else : ?>
+        <?php get_template_part('loop-templates/content', 'none'); ?>
+    <?php endif; ?>
+    <!-- end of posts  -->
+	</div>
+	</div>
+<?php
+get_footer();
