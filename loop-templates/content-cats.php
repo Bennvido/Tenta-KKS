a22bc7619e3a39fd7a444fcb9de0a551982953be
bennvido <marcus.bennvid@medieinstitutet.se>
2020-05-18 23:48:44 +0200

    Ändra så den skriver ut från custom fields
    Ändra så den skriver ut från custom fields

--- loop-templates/content-cats.php
+++ loop-templates/content-cats.php
@@ -1,6 +1,9 @@
-
+<?php
+    $image = geT_field('cat_image');
+?>
 <div class="card-body col-4">
-    <a id="cats-link-<?php the_ID(); ?>" href="<?php the_permalink(); ?>"><h1 class="text-center"><?php the_title(); ?></a>
+    <a id="cats-link-<?php the_ID(); ?>" href="<?php the_permalink(); ?>"><h1 class="text-center"><?php the_field('cat_name'); ?></h1></a>
+    <img src="<?php echo $image['url']; ?>" alt="">
     <?php the_post_thumbnail(); ?>
     <?php the_content(); ?>
 </div>
