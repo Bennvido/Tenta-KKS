01de456902937e6fcf99e1807990d362ba3ccc4a
bennvido <marcus.bennvid@medieinstitutet.se>
2020-05-18 23:47:46 +0200

    Credits ändring i footern
    Credits ändring i footern

--- footer.php
+++ footer.php
@@ -27,7 +27,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="site-info">
 
-						<?php understrap_site_info(); ?>
+					<p class="font-italic">All rights reserved by Bennvido ©2020 </p>
 
 					</div><!-- .site-info -->
 
