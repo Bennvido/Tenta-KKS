0bd734f8fa58bf2f62b6f130be75fb2a46ee7f2d
bennvido <marcus.bennvid@medieinstitutet.se>
2020-05-18 19:52:22 +0200

    Fixat hur katter skrivs ut i singel
    Fixat hur katter skrivs ut i singel

--- loop-templates/content-single-cat.php
+++ loop-templates/content-single-cat.php
@@ -1,10 +1,22 @@
-<div class="card-body col-12">
-    <a id="cat-link-<?php the_ID(); ?>" href="<?php the_permalink(); ?>"><h1 class="text-center"><?php the_title(); ?></a>
-    <?php the_post_thumbnail(); ?>
-    <?php the_content(); ?>
+<?php
+    $image = geT_field('cat_image');
+?>
 
-    <?php $link = get_post_meta($post->ID, 'Link');
-    if($link) : ?>
-        <a href="<?php echo $link; ?>">Link to this project</a>
-    <?php endif; ?>
+<div class="card mb-3 col-lg-12 mt-5">
+  <div class="row no-gutters">
+    <div class="col-lg-8 align-self-center">
+      <img src="<?php echo $image['url']; ?>" class="card-img" alt="...">
+    </div>
+    <div class="col-lg-4">
+      <div class="card-body">
+        <h5 class="card-title"><?php the_field('cat_name') ?></h5>
+        <p class="card-text"><strong>Birth date:</strong> <?php the_field('cat_age') ?></p>
+        <p class="card-text"><strong>Color:</strong> <?php the_field('cat_color') ?></p>
+        <p class="card-text"><strong>Weight:</strong> <?php the_field('cat_weight') ?></p>
+        <p class="card-text"><strong>Gender:</strong> <?php the_field('cat_gender') ?></p>
+        <p class="card-text"><strong>Description:</strong> <?php the_field('cat_description') ?></p>
+        <p class="card-text"><small class="text-muted">Updated at: <?php the_modified_date() ?></small></p>
+      </div>
+    </div>
+  </div>
 </div>
