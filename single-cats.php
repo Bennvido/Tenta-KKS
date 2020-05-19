ba3b5bcab409b98ae14631d79e78e329259e94d1
bennvido <marcus.bennvid@medieinstitutet.se>
2020-05-20 00:11:32 +0200

    Indentering
    Indentering

--- single-cats.php
+++ single-cats.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive cats
  *
@@ -8,28 +9,28 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 get_header();
 
 ?>
 <div class="container">
-	<div class="row">
-    <!-- Loop start	 -->
-    <?php if (have_posts()) : ?>
-        <!-- Yay, we have posts  -->
-        <?php while (have_posts()) : the_post(); ?>
-		
-            <!-- Accordion of FAQ's -->
-            <?php get_template_part('loop-templates/content', 'single-cat'); ?>
-            <!-- End of Accordion -->
-		
-        <?php endwhile; ?>
-    <?php else : ?>
-        <?php get_template_part('loop-templates/content', 'none'); ?>
-    <?php endif; ?>
-    <!-- end of posts  -->
-	</div>
-	</div>
+    <div class="row">
+        <!-- Loop start	 -->
+        <?php if (have_posts()) : ?>
+            <!-- Yay, we have posts  -->
+            <?php while (have_posts()) : the_post(); ?>
+
+                <!-- Accordion of FAQ's -->
+                <?php get_template_part('loop-templates/content', 'single-cat'); ?>
+                <!-- End of Accordion -->
+
+            <?php endwhile; ?>
+        <?php else : ?>
+            <?php get_template_part('loop-templates/content', 'none'); ?>
+        <?php endif; ?>
+        <!-- end of posts  -->
+    </div>
+</div>
 <?php
 get_footer();
