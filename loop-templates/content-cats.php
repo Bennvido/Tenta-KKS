167360496e11400f0c05d9e0b063befec5d98ac4
bennvido <marcus.bennvid@medieinstitutet.se>
2020-05-22 21:25:24 +0200

    skriver ut adopterad i kattarkivet
    Skriver ut om katten är adopterad eller ej i kattarkivet

--- loop-templates/content-cats.php
+++ loop-templates/content-cats.php
@@ -6,8 +6,7 @@
     <a id="cats-link-<?php the_ID(); ?>" href="<?php the_permalink(); ?>"><h1 class="text-center"><?php the_field('cat_name'); ?></h1></a>
     <img  src="<?php echo $image['url']; ?>" alt="">
     
-    
-    
     <p class="card-text"><strong>Located at:</strong> <?php echo get_the_term_list( $post->ID, 'city') ?></p>
     <p class="card-text"><strong>Gender:</strong> <?php echo get_the_term_list( $post->ID, 'gender') ?></p>
+    <p class="card-text"><strong>Adopted:</strong> <?php the_field('cat_adopted') ?>
 </div>
