99a52aca3d40d51b2c3b8864ff9aff7f3458febf
bennvido <marcus.bennvid@medieinstitutet.se>
2020-05-22 23:56:53 +0200

    Update content-usp-faq.php

--- loop-templates/content-usp-faq.php
+++ loop-templates/content-usp-faq.php
@@ -2,10 +2,8 @@
     defined('ABSPATH') || exit;
 ?>
 <div class="col-lg-4 col-md-4 col-12">
-    <div class="card">
         <div class="card-body">
             <h5 class="card-title text-center"><?php the_title(); ?></h5>
             <p class="card-text"><?php the_content(); ?></p>
         </div>
-    </div>
 </div>
