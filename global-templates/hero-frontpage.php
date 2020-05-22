5a9ae94af958b600d4819327445a858b96820e17
bennvido <marcus.bennvid@medieinstitutet.se>
2020-05-22 22:52:48 +0200

    Fixat klart allt med hero
    hero title och subtitle gömda om ej ifyllda samt max height på hero img.

--- global-templates/hero-frontpage.php
+++ global-templates/hero-frontpage.php
@@ -11,11 +11,15 @@ $bg_color = get_field('hero_background_color');
 $link = get_field('hero_link');
 ?>
 <section id="front-page-hero" style="background-color: <?php echo $bg_color; ?> ">
-    <div class="container">
+    <div class="container text-center">
+        <?php if( the_field('hero_title', 'hero_subtitle')) : ?>
+            <h1><?php the_field('hero_title')?></h1>
+            <p><?php the_field('hero_subtitle')?></p>
+        <?php endif;?>
         <?php if ( $link ) : ?>
             <a href="<?php echo $link['url']; ?>">
         <?php endif; ?>
-                <img src="<?php echo $image['url']; ?>" alt="">
+                <img style="max-height: 400px;"src="<?php echo $image['url']; ?>" alt="">
             </a>
     </div>
 </section>
