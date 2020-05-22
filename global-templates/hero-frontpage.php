50e53767771f862ee7c1ed8e1077b3dc5b2c5140
bennvido <marcus.bennvid@medieinstitutet.se>
2020-05-22 22:26:28 +0200

    Lagt till länk till  hero image
    Lagt till länk till  hero image i bilden om där är en länk

--- global-templates/hero-frontpage.php
+++ global-templates/hero-frontpage.php
@@ -6,11 +6,16 @@ defined('ABSPATH') || exit;
 ?>
 
 <?php
-    $image = geT_field('hero_image');
-    $bg_color = geT_field('hero_background_color');
+$image = get_field('hero_image');
+$bg_color = get_field('hero_background_color');
+$link = get_field('hero_link');
 ?>
 <section id="front-page-hero" style="background-color: <?php echo $bg_color; ?> ">
     <div class="container">
-        <img src="<?php echo $image['url']; ?>" alt="">
+        <?php if (get_field('hero_link')) : ?>
+            <a href="<?php echo $link['url']; ?>">
+        <?php endif; ?>
+                <img src="<?php echo $image['url']; ?>" alt="">
+            </a>
     </div>
 </section>
