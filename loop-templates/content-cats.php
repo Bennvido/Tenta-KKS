ffe6b2f9173eca09953866241218db1b0bc8cf93
bennvido <marcus.bennvid@medieinstitutet.se>
2020-05-22 23:56:11 +0200

    Fixat så att bild oavsett storlek blir bra
    Fixat så att bild oavsett storlek blir bra

--- loop-templates/content-cats.php
+++ loop-templates/content-cats.php
@@ -1,12 +1,17 @@
 <?php
-    $image = get_field('cat_image');
- 
+$image = get_field('cat_image');
+
 ?>
-<div class="card-body col-12 col-md-4 col-lg-4">
-    <a id="cats-link-<?php the_ID(); ?>" href="<?php the_permalink(); ?>"><h1 class="text-center"><?php the_field('cat_name'); ?></h1></a>
-    <img  src="<?php echo $image['url']; ?>" alt="">
-    
-    <p class="card-text"><strong>Located at:</strong> <?php echo get_the_term_list( $post->ID, 'city') ?></p>
-    <p class="card-text"><strong>Gender:</strong> <?php echo get_the_term_list( $post->ID, 'gender') ?></p>
-    <p class="card-text"><strong>Adopted:</strong> <?php the_field('cat_adopted') ?>
+<div class="card-body col-12 col-md-4 col-lg-4 text-center">
+    <a id="cats-link-<?php the_ID(); ?>" href="<?php the_permalink(); ?>">
+        <h1><?php the_field('cat_name'); ?></h1>
+    </a>
+    <div>
+        <img style="max-height: 200px; width:auto;" src="<?php echo $image['url']; ?>" alt="">
+    </div>
+    <div class="d-inline-block text-left">
+        <p class="card-text"><strong>Located at:</strong> <?php echo get_the_term_list($post->ID, 'city') ?></p>
+        <p class="card-text"><strong>Gender:</strong> <?php echo get_the_term_list($post->ID, 'gender') ?></p>
+        <p class="card-text"><strong>Adopted:</strong> <?php the_field('cat_adopted') ?>
+    </div>
 </div>
